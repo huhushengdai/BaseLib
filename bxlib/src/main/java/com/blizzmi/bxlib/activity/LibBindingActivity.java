e8d8dd0adc514bd0deae4faaaed8c967c408e6b3
wanglz <wanglz@blizzmi.com>
2017-03-14 16:12:22 +0800

    权限申请修改:在libActivity中添加申请权限方法

--- bxlib/src/main/java/com/blizzmi/bxlib/activity/LibBindingActivity.java
+++ bxlib/src/main/java/com/blizzmi/bxlib/activity/LibBindingActivity.java
@@ -2,11 +2,15 @@ package com.blizzmi.bxlib.activity;
 
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.blizzmi.bxlib.annotation.LayoutId;
+import com.blizzmi.bxlib.utils.PermissionUtils;
+
+import java.util.ArrayList;
 
 /**
  * Date： 2016/9/8
@@ -81,5 +85,38 @@ public abstract class LibBindingActivity<T extends ViewDataBinding> extends AppC
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    /**
+     * 请求权限
+     * @param requestCode 权限亲求码
+     * @param permission 需要的权限
+     */
+    public void requirePermission(int requestCode, String... permission) {
+        if (Build.VERSION.SDK_INT < 23 ||permission == null) {
+            //如果sdk 低于23 即手机版本不是6.0以上，则不需要申请
+            return;
+        }
+        //查询是否已经获取该权限
+        boolean[] isGranted = PermissionUtils.isGranted(this, permission);
+        int size = isGranted.length;
+        ArrayList<String> p = new ArrayList<>(permission.length);//需要申请的权限
+        for (int i = 0; i < size; i++) {
+            if (!isGranted[i]) {
+                //如果没有获取，则申请该权限
+                p.add(permission[i]);
+                PermissionUtils.requestPermissions(this, requestCode, permission[i]);
+            }
+        }
+        PermissionUtils.requestPermissions(this, requestCode, (String[]) p.toArray());
+    }
+
+    /**
+     * 是否获取授权
+     * @param permission 申请权限
+     * @return true  获取授权
+     */
+    public boolean isGranted(String permission){
+        return PermissionUtils.isGranted(this, permission);
     }
 }
